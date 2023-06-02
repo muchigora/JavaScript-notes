218fdc432371dfada2538bf7028bc07263bcc3b3
Muchi Gora <muchigora@gmail.com>
2023-06-02 02:27:37 -0400

    Lab 2 Completed

---
+++ js/05-functions/LAB-308H-5.3-pass-by-value-refernce/solution.js
@@ -0,0 +1,34 @@
+
+/***
+ * PASS BY VALUE && PASS BY REFERENCE
+ */
+
+
+const personOne = {
+    name: {
+        first: 'Timmy',
+        last: 'Timtim'
+    },
+    age: 30,
+    location: {
+        city: 'New York',
+        state: 'New York',
+        zip: 10001
+    }
+}
+
+const personTwo = {
+    name: {
+        first: 'Julie',
+        last: 'July'
+    },
+    age: 28,
+    location: {
+        city: 'Albany',
+        state: 'New York',
+        zip: 12201
+    }
+}
+
+
+
