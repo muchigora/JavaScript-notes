fd864e41d936327071da483e158886ab84bfb9ec
Muchi Gora <muchigora@gmail.com>
2023-06-08 14:35:21 -0400

    LAB308-5.4

---
+++ js/12-DOM/app.js
@@ -0,0 +1,8 @@
+let titleEl = document.getElementById('#title');
+console.log(titleEl);
+
+let pEl = document.querySelector(".cool");
+console.log(pEl);
+
+const newLi = document.createElement("li");
+newLi.innerText = "newLi appended!";
