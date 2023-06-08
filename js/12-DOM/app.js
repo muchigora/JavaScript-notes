0718b2b025b13290aa3e39464076d87057b41258
Muchi Gora <muchigora@gmail.com>
2023-06-08 15:46:58 -0400

    DOM

--- js/12-DOM/app.js
+++ js/12-DOM/app.js
@@ -1,8 +1,14 @@
-let titleEl = document.getElementById('#title');
-console.log(titleEl);
+console.log("test")
+
+let titleEl = document.querySelector('#newId');
+titleEl.style.color = "red";
+
 
 let pEl = document.querySelector(".cool");
 console.log(pEl);
 
 const newLi = document.createElement("li");
 newLi.innerText = "newLi appended!";
+
+pEl.style.color = "red";
+
