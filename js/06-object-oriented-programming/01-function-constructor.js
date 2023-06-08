6955c8ff946411b1cfd510b449c97ad28c63a782
Muchi Gora <muchigora@gmail.com>
2023-06-08 10:02:36 -0400

    chaining protostypes

--- js/06-object-oriented-programming/01-function-constructor.js
+++ js/06-object-oriented-programming/01-function-constructor.js
@@ -10,18 +10,11 @@ let Person = function(name, birthYear, gender){
     this.birthYear = birthYear;
     this.gender = gender;
     //methods
-    // this.calculateAge = function(){
-    //     let age = new Date().getFullYear() - this.birthYear;
-    //     console.log(age);
-    // }
+    this.calculateAge = function(){
+        let age = new Date().getFullYear() - this.birthYear;
+        console.log(age);
+    }
 }
-
-// CREATE PROTOTYPE PROPERTY FOR PERSON OBJECT
-Person.prototype.calculateAge = function(){
-    let age = new Date().getFullYear() - this.birthYear;
-    console.log(`age: ${age}`);
-}
-
 //Instantiating instances of the class
 let john = new Person("John",1985,"M");
 john.calculateAge();
@@ -32,8 +25,3 @@ console.log("-------------------------------------------------------------------
 let mary = new Person("Mary",1998, "F");
 mary.calculateAge();
 console.log(mary);
-
-console.log("--------------------------------------------------------------------------------")
-let steve = new Person("Steve",1989, "M");
-steve.calculateAge();
-console.log(steve);
---
+++ js/06-object-oriented-programming/11.1-chaining-prototypes.js
@@ -0,0 +1,9 @@
+let mark = {
+    name: 'Mark',
+    birthyear: 1992,
+    gender: 'M'
+}
+
+// console.log(mark instanceof Object);
+console.log(mark.hasOwnProperty('name'))
+console.log(mark.toString());
