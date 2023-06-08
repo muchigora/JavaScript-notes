25588d2591b4a9e83f0f845c28e27f6feabade13
Muchi Gora <muchigora@gmail.com>
2023-06-08 10:20:26 -0400

    all folders created

--- js/06-object-oriented-programming/11.1-chaining-prototypes.js
+++
@@ -1,9 +0,0 @@
-let mark = {
-    name: 'Mark',
-    birthyear: 1992,
-    gender: 'M'
-}
-
-// console.log(mark instanceof Object);
-console.log(mark.hasOwnProperty('name'))
-console.log(mark.toString());
