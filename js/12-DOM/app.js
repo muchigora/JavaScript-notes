0192a863a3f3b614522c8d32b8a35a26c0583963
Muchi Gora <muchigora@gmail.com>
2023-06-09 10:08:59 -0400

    DOM-methods

--- js/12-DOM/app.js
+++ js/12-DOM/app.js
@@ -1,14 +1,34 @@
-console.log("test")
+// console.log("test")
 
-let titleEl = document.querySelector('#newId');
+let titleEl = document.querySelector('#heading');
 titleEl.style.color = "red";
 
 
 let pEl = document.querySelector(".cool");
-console.log(pEl);
+pEl.style.color = "blue";
+
 
 const newLi = document.createElement("li");
 newLi.innerText = "newLi appended!";
 
-pEl.style.color = "red";
+window.onload = () => {
+    const heading = document.createElement("h1");
+    const headingText = document.createTextNode("Onload Heading!");
+    heading.appendChild(headingText);
+    document.body.appendChild(heading);
+}
+
+/****************************ACCESSING Document Elements************************ */
+
+//getElementById("id-name");
+let paragraph = document.getElementById("intro");
+console.log(paragraph);
+
+//querySelector();
+let div = document.querySelector(".list");
+console.log(div);
+
+let h1 = document.querySelector("#heading");
+console.log(h1);
+
 
