c5ee15192de1698fed2a6db6ee8fdd71bfea91e4
Muchi Gora <muchigora@gmail.com>
2023-06-12 09:50:16 -0400

    DOM-in-progress

--- js/12-DOM/app.js
+++ js/12-DOM/app.js
@@ -22,13 +22,64 @@ window.onload = () => {
 
 //getElementById("id-name");
 let paragraph = document.getElementById("intro");
-console.log(paragraph);
+// console.log(paragraph);
 
 //querySelector();
 let div = document.querySelector(".list");
-console.log(div);
+// console.log(div);
 
 let h1 = document.querySelector("#heading");
-console.log(h1);
+// console.log(h1);
 
+let img = document.querySelector("img");
+// console.log(img);
+
+let pgph = document.querySelectorAll("p");
+// console.log(pgph);
+
+
+
+
+
+/***********************************READING ELEMENTS DATA************************* */
+//textContent();
+let display = document.querySelector(".cool").textContent;
+console.log(display);
+
+
+//innerHTML();
+let divDisplay = document.querySelector(".list").innerHTML;
+console.log(divDisplay);
+
+
+
+/****************Modifying Document Elements ********************************/
+document.querySelector("#dynamicContent").textContent = "This is dynamically modified paragraph"
+
+div.innerHTML = `
+                
+                <table border="1" cellspacing="2px" cellpadding="5px">
+                <header>
+                    <strong>
+                        Changed Content from list to table
+                    </strong>
+                </header>
+                    <tr>
+                        <td>1</td>
+                        <td>Accessing DOM element</td>
+                    </tr> 
+                    <tr>
+                        <td>2</td>
+                        <td>Modifying DOM element</td>
+                    </tr> 
+                    <tr>
+                        <td>3</td>
+                        <td>Adding new DOM element</td>
+                    </tr> 
+                    <tr>
+                        <td>4</td>
+                        <td>Removing existing DOM element</td>
+                    </tr> 
+                </table>              
+                `
 
