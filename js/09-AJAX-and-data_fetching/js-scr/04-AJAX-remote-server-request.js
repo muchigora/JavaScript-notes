3b598bfe996b17d78bc33d875381c6d9283ec1a8
Muchi Gora <muchigora@gmail.com>
2023-06-16 16:32:08 -0400

    commit

---
+++ js/09-AJAX-and-data_fetching/js-scr/04-AJAX-remote-server-request.js
@@ -0,0 +1,41 @@
+/*****************AJAX REQUEST TO A WEB API ************************************/
+
+let countriesContainer = document.querySelector(".countries");
+
+function getCountry(countryName){
+    //Step1: create an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+
+
+    //Step2: create Request with open() method
+    xhr.open("GET", "https://restcountries.com/v3.1/name/"+countryName, true);
+
+    //Step3: send request send() method
+    xhr.send();
+
+    //Step4: load event handling
+    xhr.addEventListener("load", function(){
+        // console.log(xhr.responseText); //test data in console
+        //convert JSON Data to JavaScript Object
+        let [data] = JSON.parse(xhr.responseText);
+        // let countryData = data[0]
+        
+        console.log(data);
+
+        //create html template for diplaying this data
+        let html = `
+            <article class="country">
+                <img class="country_img" src="${data.flags}"/>
+                <div class="country_data">
+                    <h3 class="country_name">${data.name}</h3>
+                    <h4 class="country_region">${data.region}</h4>
+                    <p class="country_row"><span class="ion-ios-people"></span>${data.population}</p>
+                    <p class="country_row"><span class="ion-ios-mic">${data.languages}</span>
+                    <p class="country_row"><span class="ion-social-usd>${data.currencies}</span>
+                </div>
+            </article>
+        `;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+    });
+}
+getCountry("usa")
